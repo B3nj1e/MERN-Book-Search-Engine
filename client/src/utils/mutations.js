9627a6ac529d16f15bf9162112431d6811d9a16c
Benjamin Clewer <benjiclewer@gmail.com>
2022-02-04 11:36:49 +1030

    adding mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,46 @@
+import { gql } from '@apollo/client';
+
+export const SAVE_BOOK = gql`
+  mutation saveBook: (author: String!, description: String!, bookId: String!, image: String!, image: String!, link: String!, title: String!) {
+    saveBook(author: $author, description: $description, bookId: $bookId, image: $image, link: $link, title: $title) {
+      token
+      user {
+        savedBooks
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook(bookId: String!) {
+    removeBook(bookId: $bookId) {
+    token
+      user {
+        savedBooks
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email:$email) {
+      email
+      username
+      password
+      savedBooks
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        username
+        savedBooks
+      }
+    }
+  }
+`;
